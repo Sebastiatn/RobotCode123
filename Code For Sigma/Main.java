4e54161aac1591894b3c411421efadd77802d0cc
Sebastiatn <sebastiant87@nycstudents.net>
2025-06-04 23:43:18 -0400

    Update Main.java

--- Code For Sigma/Main.java
+++ Code For Sigma/Main.java
@@ -1,29 +1,33 @@
 package p3Seb;
 
 public class Main {
-	public static void main(String[] args) {
-		String up = "f";
-		int[] led = {0,0,0,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,0,0,0,0,0};
-		Finch bird = new Finch();
-		bird.setDisplay(led);	
-		while(bird.isShaking() == false) {
-			System.out.println("Compass Heading: " + bird.getCompass());
-			bird.setMove("F", 999, 50);
-			bird.pause(10);
-			if(bird.distance)
-		}
-	}
-	public void setDisplay(int[] led, int set, int value) {
-		int count = 0;
-		int setValue = set;
-		for(int i = 0; i < led.length; i++) {
-			count++;
-			if(count == setValue) {
-				led[i] = value;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Finch bird = new Finch();
+        int[] led = new int[25];
+        System.out.print("Enter an led position to turn on (1 to 25) and it must me either a 1 or a 0: ");
+        int userInput = scanner.nextInt();
+        if (userInput < 1 || userInput > 25) {
+            System.out.println("Invalid input. Please enter a number from 1 to 25.");
+            return;
+        }
+        setDisplay(led, userInput, 1);
+        bird.setDisplay(led);
+        while (!bird.isShaking()) {
+            System.out.println("Compass Heading: " + bird.getCompass());
+            bird.setMove("F", 999, 50);
+            bird.pause(10);
+        }
+        
+        bird.setDisplay(new int[25]);
+    }
+    public static void setDisplay(int[] led, int set, int value) {
+        if (set >= 1 && set <= led.length) {
+            led[set - 1] = value; 
+        }
+    }
 }
+
 /*while(!(up.equals("f"))) {
 System.out.print("Distance to obstacle: " + bird.getDistance());
 bird.setMove("F",999,50);
